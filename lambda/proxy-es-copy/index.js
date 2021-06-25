ca8c831a60affd439a9ba3006aded6319cf37fec
Karl Thomas <thokrl+github@amazon.com>
2021-06-24 20:21:02 -0400

    added alexa.js

---
+++ lambda/proxy-es-copy/index.js
@@ -0,0 +1,28 @@
+
+const filter = text => {
+    if (process.env.CLOUDWATCHLOGGINGDISABLED === "true"){
+        return "cloudwatch logging disabled";
+    } else {
+        if (process.env.QNAREDACT === "true") {
+            let re = new RegExp(process.env.REDACTING_REGEX, "g");
+            return text.replace(re, "XXXXXX");
+        } else {
+            return text;
+        }
+    }
+}
+
+require('intercept-stdout')(filter, filter);
+
+exports.qid=require('./lib/qid')
+exports.logging=require('./lib/logging')
+exports.cleanmetrics=require('./lib/cleanmetrics')
+exports.utterances=require('./lib/utterances')
+exports.handler =require('./lib/handler') 
+
+exports.query=function(event,context,callback){
+    require('./lib/query')(event.req,event.res)
+    .then((x)=>callback(null,x)) 
+    .catch(callback)
+}
+
